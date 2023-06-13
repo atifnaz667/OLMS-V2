3fbd1506dda82c6386f9ca6f6e04c4e30ee04fa1
daniesh <danishtahironly@gmail.com>
2023-06-13 22:50:21 +0500

    nsert', ['link', 'picture'

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -169,7 +169,7 @@
                     ['color', ['color']],
                     ['para', ['ul', 'ol', 'paragraph']],
                     ['table', ['table']],
-                    // ['insert', ['link', 'picture', 'video']],
+                    ['insert', ['link', 'picture', 'video']],
                     // ['view', ['fullscreen', 'codeview', 'help']]
                 ]
             });
